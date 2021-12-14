5a5a2eb2514e4165e53a10f3e2c625683ff8e0bf
Javaun Wright <javaun.wright@salesforce.com>
2021-12-14 15:10:24 -0500

    updates.

---
+++ JAW_package_manager.py
@@ -0,0 +1,59 @@
+#!/usr/local/bin/python3
+
+import yaml
+import urllib.request
+import pprint
+# Running cumulus task to update dependencies.
+
+test_yml = "https://raw.githubusercontent.com/GeekStewie/universities/master/test.yml"  # yml package
+filename = 'jaw_test_dependencies.yml'
+
+
+def get_yml_pkg(yml_url):  # return a dictionary with package information from a url.
+    captured_yml = urllib.request.urlopen(yml_url)
+    '''Capture package section in cumulus.yml'''
+    return yaml.safe_load(captured_yml)
+
+
+def load_cumulus_yml():
+    with open('cumulusci.yml', 'r') as f:
+        cumulus_yml = yaml.safe_load(f)
+        return cumulus_yml
+
+
+def update_cumulus_file(yml_data, dependency_data):
+    yml_data['tasks']['update_dependencies']["options"] = dependency_data['project']['dependencies']
+
+
+def write_yml_pkg(yml_file, yml_data):  # Add dependency data to cumulus.yml file
+    with open(yml_file, "w") as f:
+        '''Updating cumulus.yml with github dependencies'''
+        f.write(yaml.safe_dump(yml_data))
+
+
+raw_yml = get_yml_pkg(test_yml)
+cumulus_data = load_cumulus_yml()
+update_cumulus_file(cumulus_data, raw_yml)
+write_yml_pkg('filename.yml', cumulus_data)
+
+
+pprint.pprint(cumulus_data)
+# ---------------------
+
+# command: "cci task run update_dependencies --dependencies test.yml"
+# Notes section:
+
+
+# Questions:
+# - How do i store a list of packages externally?
+# -- YML File hosted on github
+#
+# - What does DynamicDependency class in cumulus do?
+#
+# - How can I check for dependencies?
+# -- The update_dependencies task
+
+## Resources:
+# test yml: https://raw.githubusercontent.com/GeekStewie/universities/master/test.yml
+# urllib module: https://www.geeksforgeeks.org/python-urllib-module/
+# Python YAML: https://pyyaml.org/wiki/PyYAMLDocumentation
--- remote_site_script.py
+++ remote_site_script.py
@@ -3,7 +3,6 @@ import os
 import re
 import argparse
 
-
 parser = argparse.ArgumentParser(description='Deploy Remote site.')
 parser.add_argument('-o', '--org', dest="usr_org", help="Specify org name to deploy remote site settings.")
 args = parser.parse_args()
@@ -11,13 +10,12 @@ args = parser.parse_args()
 Org = args.usr_org  # Storing the users org.
 
 
-def capture_instance_url(org_name): # Capturing Instance URL  current orgs instance_url.
-    stream = os.popen('cci org info {}'.format(org_name)).read() # Running Command to display our instance url.
-
-    re_instance_url = re.search("(instance_url).+(https.+m)", stream) # Regex that captures our instance.
+def capture_instance_url(org_name):  # Capturing Instance URL  current org's instance_url.
+    stream = os.popen('cci org info {}'.format(org_name)).read()  # Running Command to display our instance url.
+    re_instance_url = re.search("(instance_url).+(https.+m)", stream)  # Regex that captures our instance.
     instance_url = re_instance_url.group(2)
     return instance_url
-    
+
 
 def create_deploy_directory(directory_path, xml_path, remote_site_xml):
     try:
@@ -25,8 +23,7 @@ def create_deploy_directory(directory_path, xml_path, remote_site_xml):
     except FileExistsError:
         # directory already exists
         pass
-
-    with open(xml_file_path, "w") as f:
+    with open(xml_path, "w") as f:
         f.write(remote_site_xml)
 
 
@@ -38,7 +35,8 @@ xml_data = """<?xml version="1.0" encoding="UTF-8"?>
     <isActive>true</isActive>
     <url>{}</url>
 </RemoteSiteSetting>
-""".format(deploy_data) 
+""".format(deploy_data)
+
 
 # Setting up file path and xml file name.
 deploy_directory_path = "sfdo_temp/remoteSiteSettings"
@@ -46,20 +44,18 @@ xml_file = "sfdo_trusted_site.remoteSite-meta.xml"
 xml_file_path = "{}/{}".format(deploy_directory_path, xml_file)
 
 create_deploy_directory(deploy_directory_path, xml_file_path, xml_data)
-os.popen( "cci task run deploy_pre --path sfdo_temp/ --org {}".format(Org)) # cci task run deploy_pre --path sfdo_temp/ --org dev
-
-# Notes
-
-# OS file path for temp dir: /Users/javaun.wright/workspace/projects/JAW_work/jaw_nonprofit/sfdo_temp/remoteSiteSettings
-
-# Pre-req - pass in org name as argument variable.
-
-# 1. Generate xml data into temp folder: ./sfdo_temp/remoteSiteSettings/ 
-# 2. rename xml : LabelHere.remoteSite-meta.xml # LabelHere = any name
-# 3. cci task run deploy --path force-app --org OrgNameHere #-- Point deploy to this folder.
 
 
-# Notes May need package.xml file - incase reading error.
-#   cci task run update_package_xml --path sfdo_temp/ --org dev
 
-
+# flows:
+    # deploy_remote_site:
+        # description: Add our site into our remote site list.
+        # steps:
+            # 1:
+                # task: command
+                # options:
+                # command: ./remote_site_script.py --org dev
+            # 2:
+                # task: deploy_pre
+                # options:
+                # path: sfdo_temp/
